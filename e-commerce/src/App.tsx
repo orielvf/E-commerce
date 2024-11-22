1c2ccc2fc388f44edafaf71f6f48fa0b4e7aaf93
orielvf <orielvf@gmail.com>
2024-11-22 03:13:09 -0300

    feat: item

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,29 +1,30 @@
 //Import styles
 import './App.css'
+//Import Swiper components
+import Slider from './components/Slider/Slider';
+import { SwiperProps, SwiperSlide } from 'swiper/react';
 //Import React components
 import Banner from './components/Banner';
 import Header from './components/Header';
-//Import Swiper components
-import Slider from './components/Slider/Slider';
-import {SwiperProps, SwiperSlide} from 'swiper/react';
+import Item from './components/Item/Item';
 
 
 export default function App() {
-  const settings : SwiperProps= {
-    slidesPerView :1,
-    spaceBetween :30,
-    loop :true,
+  const settings: SwiperProps = {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
     //pagination: false,// {clickable: true},
-    pagination: {clickable: true},
-    navigation :true, 
+    pagination: { clickable: true },
+    navigation: true,
     //a11y :true,
   };
 
   return (
     <div>
-      <Header/>
-      <Banner/>
-      <Slider settings = {settings}>
+      <Header />
+      <Banner />
+      <Slider settings={settings}>
         <SwiperSlide>
           <h1>teste 1</h1>
         </SwiperSlide>
@@ -36,8 +37,9 @@ export default function App() {
         <SwiperSlide>
           <h1>teste 4</h1>
         </SwiperSlide>
-    </Slider>
+      </Slider>
+      <Item />
     </div>
-    
+
   )
 }
---
+++ e-commerce/src/components/Item/Item.tsx
@@ -0,0 +1,47 @@
+import './Item.css';
+import { Carrinho, MenuCelular, Caminhao, Coracao, CarrinhoInserir } from './index';
+
+interface ItemProps {
+}
+
+export default function Item({ }: ItemProps) {
+    return (
+        <div id="Item">
+
+            <div id="Item-top">
+                <div id="Item-top-promoAtual">
+                    <span>promoAtual</span>
+                </div >
+                <div id="Item-top-coracaoEcarrinho">
+                    <Coracao className="hover" />
+                    <CarrinhoInserir className="hover" />
+                </div>
+                <div id="Item-top-estrela">
+                    <span className="default">estrela</span>
+                </div>
+            </div>
+
+            <div id="Item-img">
+                <img src="https://images.kabum.com.br/produtos/fotos/320797/processador-amd-ryzen-7-5700x-cache-36mb-3-8ghz-4-6ghz-max-turbo-am4-100-100000926wof_1647636511_m.jpg"
+                    alt="img" />
+            </div>
+            <div id="Item-botton">
+                <div id="Item-botton-div-frete-gratis">
+                    <Caminhao id="Item-botton-caminhao"></Caminhao>
+                    <span>Frete grátis*</span>
+                </div>
+                <p>descricao</p>
+                <p>precoAntigo</p>
+                <p>precoAtual</p>
+                <p>desconto</p>
+                <p>parcela</p>
+                <p>fidelidade</p>
+            </div>
+            <div id="Item-comprar">
+                <Carrinho id="Item-comprar-carrinho"></Carrinho>
+                <span>COMPRAR</span>
+            </div>
+        </div>
+    )
+
+}
