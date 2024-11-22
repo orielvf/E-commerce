5a9a4f8b395fc72d812e87bb213a74190d4dd393
orielvf <orielvf@gmail.com>
2024-11-22 03:13:46 -0300

    style: item css

--- e-commerce/src/components/Header.tsx
+++ e-commerce/src/components/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+//import icons
 import { AiOutlineMenu } from "react-icons/ai";
 import { BsCart2 } from "react-icons/bs";
 
 const Header: React.FC = () => {
   return (
-    <header style={{ 
-      backgroundColor: 'black', 
+    <header style={{
+      backgroundColor: 'black',
       width: '100%',
-      position: 'fixed', 
-      top: 0, 
+      position: 'fixed',
+      top: 0,
       left: 0,
       display: 'flex',
       justifyContent: 'space-between',
@@ -26,25 +27,27 @@ const Header: React.FC = () => {
       <a style={{ color: 'white', fontSize: '1.2rem', textDecoration: 'none' }}>Kabum</a>
 
       {/* Barra de Busca */}
-      <input 
-        placeholder="Busque aqui" 
-        style={{ 
-          padding: '0.5rem', 
-          width: '40%', 
+      <input
+        placeholder="Busque aqui"
+        style={{
+          padding: '0.5rem',
+          width: '40%',
           maxWidth: '300px'  // Limita a largura do input
-        }} 
+        }}
       />
 
       {/* Link de Login */}
-      <a style={{ color: 'white'
+      <a style={{
+        color: 'white'
       }}>ENTRE ou CADASTRE-SE</a>
 
       {/* Ícone do Carrinho */}
-      <BsCart2 style={{ color: 'white',
+      <BsCart2 style={{
+        color: 'white',
         marginRight: '10px', // Adiciona margem para espaçamento
         width: '15%'
-      }}/>
-      
+      }} />
+
     </header>
   );
 };
---
+++ e-commerce/src/components/Item/index.ts
@@ -0,0 +1,11 @@
+import { AiOutlineMenu } from "react-icons/ai";//menu de celular
+import { BsCart2 } from "react-icons/bs";//carrinho
+import { BsCartPlus } from "react-icons/bs";//Inserir no carrinho
+import { FaTruck } from "react-icons/fa";//caminhão
+import { IoIosHeartEmpty } from "react-icons/io";//coração
+
+export { BsCart2 as Carrinho }
+export { BsCartPlus as CarrinhoInserir }
+export { AiOutlineMenu as MenuCelular }
+export { FaTruck as Caminhao }
+export { IoIosHeartEmpty as Coracao }
