7e8a659bc34b4c37dff5b082d46a98ebff026350
orielvf <orielvf@gmail.com>
2024-12-02 02:55:33 -0300

    style: changes to the banner

--- e-commerce/src/components/Banner/Banner.tsx
+++ e-commerce/src/components/Banner/Banner.tsx
@@ -1,29 +1,57 @@
-import { SwiperProps, SwiperSlide } from 'swiper/react';
-import Slider from '../Slider/Slider';
+import './Banner.css';
 import mock from '../../../components-mock.json'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper React components
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 
 export default function Banner() {
 
-    const settings: SwiperProps = {
+    /*const settings: SwiperProps = {
         slidesPerView: 1,
         spaceBetween: 1,
         loop: true,
         //pagination: false,// {clickable: true},
         pagination: { clickable: true },
         navigation: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false
+        }
         //a11y :true,
     };
-
+    settings={settings}
+*/
     return (
         <div id="Banner">
-            <Slider settings={settings}>
+            <Swiper
+                spaceBetween={1}
+                slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
                 {mock.banersMock.map((item, index) => (
                     <SwiperSlide key={index} className='item'>
-                        <img src={item.endereco} alt="Banner" />
+                        <img src={item.endereco} alt="banner" />
                     </SwiperSlide>
                 ))}
-            </Slider>
+            </Swiper>
         </div>
     )
 }
--- e-commerce/src/components/Slider/Slider.tsx
+++ e-commerce/src/components/Slider/Slider.tsx
@@ -1,6 +1,6 @@
 // Import Swiper components
 import { Swiper, SwiperProps } from 'swiper/react';
-import { Pagination, Navigation, A11y } from 'swiper/modules';
+import { Pagination, Navigation, A11y, Autoplay } from 'swiper/modules';
 // Import styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -15,7 +15,7 @@ interface SliderProps {
 }
 
 export default function Slider({ settings, children }: SliderProps) {
-    return <Swiper modules={[Navigation, Pagination, A11y]} {...settings}>
+    return <Swiper modules={[Navigation, Pagination, A11y, Autoplay]} {...settings}>
         {children}
     </Swiper>;
 
